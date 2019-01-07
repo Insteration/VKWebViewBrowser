0aff617576c3fce35389b5aec227d475427ae64c
Johansson <inste@icloud.com>
2019-01-07 23:36:54 +0200

    Initial Commit

---
+++ VKWebViewBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKWebViewBrowser
+//
+//  Created by Artem Karmaz on 1/7/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
